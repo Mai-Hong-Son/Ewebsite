cb13e677fce9aedca522f7c0f2c3c433311277c7
Mai Sơn <sonmh@wefit.vn>
2017-11-25 16:43:23 +0700

    update

--- src/components/GradeThree/ChildOfRow.js
+++ src/components/GradeThree/ChildOfRow.js
@@ -1,119 +1,162 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import './GradeThree.css';
 import 'antd/dist/antd.css';
+import { Row, Col, Icon } from 'antd';
 
 class ChildOfRow extends Component {
-  render() {
-    return (
-        <div className='ehmld_box' style={{ maxHeight: this.props.maxHeight, transition: 'all 0.5s linear' }}>
-            <div className='wrap_child_box'>
-                <div className='ehmld_icon'></div>
-                <div className='ehmld_arcontent'>
-                    <div className='ehmld_tit'>
-                        VOCABULARY - TỪ VỰNG
-                    </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon'>
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            isShow: false,
+            maxHeight: 0,
+            openSectionIndex: -1,
+            colorL: '#00a651',
+            colorM: '#81c769',
+            colorR: '#6a8d5e',
+        }
+    }
 
-                        </div>
-                        <div className='ehmldn_text'>
-                            <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17356-chu-de-noi-loi-chao-va-tam-biet.html">
-                                <div className="ehmldnt_up">Chủ đề nói lời chào và tạm biệt</div>
-                            </a>
-                            <div className="ehmldnt_down">(Từ vựng: hello, hi, nice to meet you, goodbye, bye, bye-bye)</div>
-                        </div>
-                    </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon'></div>
-                        <div className='ehmldn_text'>
-                            <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
-                                <div className="ehmldnt_up">Chủ đề hỏi thăm sức khỏe</div>
-                            </a>
-                            <div className="ge ehmldnt_down">(Từ vựng: fine, thanks, thank you, how are you)</div>
-                        </div>
-                    </div>
+    toggleContent() {
+        this.props.toggleOne(this.props.id);
+    }
+
+    getHeight() {
+        if(this.props.open){
+            return 468;
+        } else {
+            return 0;
+        }
+    }
+
+    renderContent( item, index ) {
+        return(
+            <div className='ehmld_name'>
+                <div className='ehmldn_icon'>
+
+                </div>
+                <div className='ehmldn_text'>
+                    <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17356-chu-de-noi-loi-chao-va-tam-biet.html">
+                        <div className="ehmldnt_up">{item.name}</div>
+                    </a>
+                    <div className="ehmldnt_down">(Từ vựng: hello, hi, nice to meet you, goodbye, bye, bye-bye)</div>
                 </div>
             </div>
-            <div className='ehmld_area_lesson' style={{ paddingTop: 20, paddingBottom: 20 , textAlign: 'left', width: '100%' }}>
-                <div className='ehmld_icon'></div>
-                <div className='ehmld_arcontent'>
-                    <div className='ehmld_tit'>
-                        LECTURE - BÀI GIẢNG
-                    </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon'>
+        );
+    }
 
-                        </div>
-                        <div className='ehmldn_text'>
-                            <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17356-chu-de-noi-loi-chao-va-tam-biet.html">
-                                <div className="ehmldnt_up">Nói lời chào khi lần đầu gặp mặt</div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon'>
+    render() {
+        const { item } = this.props;
+        const { content: { vocaburaly } } = item;
+        const renderCtVoca = _.map(vocaburaly, this.renderContent.bind(this));
 
-                        </div>
-                        <div className='ehmldn_text'>
-                        <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
-                            <div className="ehmldnt_up">Hỏi thăm sức khoẻ</div>
-                        </a>
+        return (
+            <div style={{ width: '100%', maxHeight: 500, marginBottom: 43, position: 'relative' }} onClick={this.toggleContent.bind(this)}>
+                <Row style={{ width: '95%', height: 72, margin: 'auto', cursor: 'pointer', position: 'relative' }}>
+                    <Col xs={2} sm={2} md={2} lg={2} xl={2}
+                    style={{ backgroundColor: !this.props.open ? this.state.colorL : '#8560a8', height: '100%', color: '#ffffff', fontSize: 48 }}>{item.id}</Col>
+                    <Col xs={18} sm={18} md={18} lg={18} xl={18}
+                    style={{ backgroundColor: !this.props.open ? this.state.colorM : '#ffbd0c', height: '100%', color: '#ffffff', textAlign: 'left', paddingLeft: 20 }}>
+                        <span style={{ display: 'block', fontSize: 26 }}>Unit {item.id}: {item.titleEn}</span>
+                        <span style={{ display: 'block', fontSize: 16, color: 'black' }}>Bài {item.id}: {item.titleVn}</span>
+                    </Col>
+                    <Col xs={4} sm={4} md={4} lg={4} xl={4}
+                    style={{ backgroundColor: !this.props.open ? this.state.colorR : '#a18334', height: '100%', color: '#ffffff', textAlign: 'right', paddingRight: 20, fontSize: 30, paddingTop: 15 }}>
+                        <Icon type="down" />
+                    </Col>
+                </Row>
+                <div className='ehmld_box' style={{ maxHeight: this.getHeight(), transition: 'all 0.5s linear' }}>
+                    <div className='wrap_child_box'>
+                        <div className='ehmld_icon'></div>
+                        <div className='ehmld_arcontent'>
+                            <div className='ehmld_tit'>
+                                VOCABULARY - TỪ VỰNG
+                            </div>
+                            {renderCtVoca}
                         </div>
                     </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon ehmldn_icon_pen'>
+                    <div className='ehmld_area_lesson' style={{ paddingTop: 20, paddingBottom: 20 , textAlign: 'left', width: '100%' }}>
+                        <div className='ehmld_icon'></div>
+                        <div className='ehmld_arcontent'>
+                            <div className='ehmld_tit'>
+                                LECTURE - BÀI GIẢNG
+                            </div>
+                            <div className='ehmld_name'>
+                                <div className='ehmldn_icon'>
 
-                        </div>
-                        <div className='ehmldn_text'>
-                        <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
-                            <div className="ehmldnt_up">Luyện tập tổng hợp bài giảng 1 & 2</div>
-                        </a>
+                                </div>
+                                <div className='ehmldn_text'>
+                                    <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17356-chu-de-noi-loi-chao-va-tam-biet.html">
+                                        <div className="ehmldnt_up">Nói lời chào khi lần đầu gặp mặt</div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className='ehmld_name'>
+                                <div className='ehmldn_icon'>
+
+                                </div>
+                                <div className='ehmldn_text'>
+                                <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
+                                    <div className="ehmldnt_up">Hỏi thăm sức khoẻ</div>
+                                </a>
+                                </div>
+                            </div>
+                            <div className='ehmld_name'>
+                                <div className='ehmldn_icon ehmldn_icon_pen'>
+
+                                </div>
+                                <div className='ehmldn_text'>
+                                <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
+                                    <div className="ehmldnt_up">Luyện tập tổng hợp bài giảng 1 & 2</div>
+                                </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div className='ehmld_area_phonic' style={{ paddingTop: 20, textAlign: 'left', width: '100%', marginBottom: 25 }}>
+                        <div className='ehmld_icon'></div>
+                        <div className='ehmld_arcontent'>
+                            <div className='ehmld_tit'>
+                                PHONICS - HỌC VỀ ÂM
+                            </div>
+                            <div className='ehmld_name'>
+                                <div className='ehmldn_icon'>
+
+                                </div>
+                                <div className='ehmldn_text'>
+                                    <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17356-chu-de-noi-loi-chao-va-tam-biet.html">
+                                        <div className="ehmldnt_up">Phát âm chữ "b"</div>
+                                    </a>
+                                    
+                                </div>
+                            </div>
+                            <div className='ehmld_name'>
+                                <div className='ehmldn_icon'>
+
+                                </div>
+                                <div className='ehmldn_text'>
+                                <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
+                                    <div className="ehmldnt_up">Phát âm chữ "h"</div>
+                                </a>
+                                </div>
+                            </div>
+                            <div className='ehmld_name'>
+                                <div className='ehmldn_icon ehmldn_icon_pen'>
+
+                                </div>
+                                <div className='ehmldn_text'>
+                                <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
+                                    <div className="ehmldnt_up">Bài tập phát âm chữ "b" và chữ "h"</div>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='ehml_verline' style={{ height: 369, left: 63, backgroundColor: '#8560a8', width: 2 , position: 'absolute', top: -1 }}></div>
                 </div>
             </div>
-            <div className='ehmld_area_phonic' style={{ paddingTop: 20, textAlign: 'left', width: '100%', marginBottom: 25 }}>
-                <div className='ehmld_icon'></div>
-                <div className='ehmld_arcontent'>
-                    <div className='ehmld_tit'>
-                        PHONICS - HỌC VỀ ÂM
-                    </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon'>
-
-                        </div>
-                        <div className='ehmldn_text'>
-                            <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17356-chu-de-noi-loi-chao-va-tam-biet.html">
-                                <div className="ehmldnt_up">Phát âm chữ "b"</div>
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon'>
-
-                        </div>
-                        <div className='ehmldn_text'>
-                        <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
-                            <div className="ehmldnt_up">Phát âm chữ "h"</div>
-                        </a>
-                        </div>
-                    </div>
-                    <div className='ehmld_name'>
-                        <div className='ehmldn_icon ehmldn_icon_pen'>
-
-                        </div>
-                        <div className='ehmldn_text'>
-                        <a className="eh_link" href="https://www.tienganh123.com/tieng-anh-lop3-unit1-vocab/17357-chu-de-hoi-tham-suc-khoe.html">
-                            <div className="ehmldnt_up">Bài tập phát âm chữ "b" và chữ "h"</div>
-                        </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='ehml_verline' style={{ height: 369, left: 63, backgroundColor: '#8560a8', width: 2 , position: 'absolute', top: -1 }}></div>
-        </div>
-    );
+        );
   }
 }
 
--- src/components/GradeThree/RowContent.js
+++ src/components/GradeThree/RowContent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import './GradeThree.css';
 import 'antd/dist/antd.css';
-import { Row, Col, Icon } from 'antd';
 import ChildOfRow from './ChildOfRow';
 
 class RowContent extends Component {
@@ -10,59 +9,30 @@ class RowContent extends Component {
         super(props);
         
         this.state = {
-            isShow: false,
-            maxHeight: 0,
-            colorL: '#00a651',
-            colorM: '#81c769',
-            colorR: '#6a8d5e',
+            openSectionIndex: -1,
         }
     }
 
-    ShowRowDetail = id => {
-        console.warn(id);
-        this.setState({
-            isShow: !this.state.isShow,
-        }, () => {
-            if(this.state.isShow)
-                this.setState({
-                    maxHeight: 468,
-                    colorL: '#8560a8',
-                    colorM: '#ffbd0c',
-                    colorR: '#a18334',
-                });
-            else
-                this.setState({
-                    maxHeight: 0,
-                    colorL: '#00a651',
-                    colorM: '#81c769',
-                    colorR: '#6a8d5e',
-                });
-        });
+    buildSections(sectionList) {
+        const sections = _.map(sectionList, this.buildSection.bind(this));
+        return sections;
     }
 
-    renderChildRow(item) {
-        return(
-            <div style={{ width: '100%', maxHeight: 500, marginBottom: 30, position: 'relative' }} onClick={this.ShowRowDetail.bind(this, item.id)} key={item.id}>
-                <Row style={{ width: '95%', height: 72, margin: 'auto', cursor: 'pointer', position: 'relative' }}>
-                    <Col xs={2} sm={2} md={2} lg={2} xl={2}
-                    style={{ backgroundColor: this.state.colorL, height: '100%', color: '#ffffff', fontSize: 48 }}>{item.id}</Col>
-                    <Col xs={18} sm={18} md={18} lg={18} xl={18}
-                    style={{ backgroundColor: this.state.colorM, height: '100%', color: '#ffffff', textAlign: 'left', paddingLeft: 20 }}>
-                        <span style={{ display: 'block', fontSize: 26 }}>Unit {item.id}: {item.titleEn}</span>
-                        <span style={{ display: 'block', fontSize: 16, color: 'black' }}>Bài {item.id}: {item.titleVn}</span>
-                    </Col>
-                    <Col xs={4} sm={4} md={4} lg={4} xl={4}
-                    style={{ backgroundColor: this.state.colorR, height: '100%', color: '#ffffff', textAlign: 'right', paddingRight: 20, fontSize: 30, paddingTop: 15 }}>
-                        <Icon type="down" />
-                    </Col>
-                </Row>
-                <ChildOfRow data={item} maxHeight={this.state.maxHeight}/>
-            </div>
-        );
+    buildSection(section, index) {
+        const openStatus = (index === this.state.openSectionIndex);
+        return <ChildOfRow id={index} item={section} toggleOne={this.toggleOne.bind(this)} open={openStatus} />
+    }
+
+    toggleOne(id) {
+        if(this.state.openSectionIndex === id){
+            this.setState({openSectionIndex: -1});
+        } else {
+            this.setState({openSectionIndex: id});
+        }
     }
 
     render() {
-        const lessonContent = _.map(this.props.rowData, item => this.renderChildRow(item));  
+        const sections = this.buildSections(this.props.rowData);
 
         return (
             <div>
@@ -87,7 +57,7 @@ class RowContent extends Component {
                     </div>
                     <div style={{ width: '35%', height: 1, backgroundColor: '#ff8eb2', verticalAlign: 'middle', display: 'inline-block', position: 'relative' }}></div>
                 </div>
-                {lessonContent}
+                {sections}
             </div>
         );
     }
